1854ff4348b3bc424c39f8d1398db2bdc153fe7b
nighteye1228 <victor70412@gmail.com>
2022-06-01 01:28:31 +0800

    custom data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,55 @@
 <script setup>
 import { ref, onMounted, computed } from "vue";
 import CardList from "./CardList.vue";
-import { data } from "../assets/data";
+
+const inputWord = ref('');
+const data = ref('');
+const isOpenInputText = ref(false);
 
 const cardLists = computed(() => {
-  return data.match(/##([^#]+\n)+/g);
+  return data.value.match(/##([^#]+\n)+/g);
 });
+
+function updateWord() {
+  data.value = inputWord.value;
+  isOpenInputText.value = false;
+}
 </script>
 
 <template>
-    <div class="columns has-text-left">
-        <CardList v-for="cardList in cardLists" :cardList="cardList"></CardList>
+  <div class="columns has-text-left">
+    <CardList v-for="cardList in cardLists" :cardList="cardList"></CardList>
+  </div>
+  <button class="button update-data" @click="isOpenInputText = true">
+    更新資料
+  </button>
+  <div class="modal" :class="{ 'is-active': isOpenInputText }">
+    <div class="modal-background" @click="isOpenInputText = false"></div>
+    <div class="modal-card">
+      <header class="modal-card-head has-text-left">
+        <p class="modal-card-title">輸入卡片</p>
+        <span>（以##開頭為每個清單標題，每一行字為一張卡片）</span>
+        <button class="delete" aria-label="close" @click="isOpenInputText = false"></button>
+      </header>
+      <section class="modal-card-body">
+        <textarea class="textarea" placeholder="10 lines of textarea" rows="10" v-model="inputWord"></textarea>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success" @click="updateWord">Save changes</button>
+        <button class="button" @click="isOpenInputText = false">Cancel</button>
+      </footer>
     </div>
+  </div>
 </template>
 
 <style scoped>
 .columns {
   overflow: scroll;
+}
+.update-data {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  z-index: 3;
 }
 </style>

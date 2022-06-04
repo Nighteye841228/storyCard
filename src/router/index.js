36f72a2ff4e5b5abcf786acde9c802fa41b397fc
nighteye1228 <victor70412@gmail.com>
2022-06-05 02:53:00 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HelloWorld from '../components/HelloWorld.vue';
+import HelloWorld from "../components/HelloWorld.vue";
 import { content } from "../assets/data";
 
 // 創建一個 routes 陣列，定義所有 route 路徑
@@ -9,12 +9,12 @@ const routes = [
     path: "/teenage-is-wrong",
     component: HelloWorld,
     props: { content: content },
-  },
+  }
 ];
 
 // 創建一個router，
 const router = createRouter({
-  history: createWebHistory('/storyCard/'), // 這邊我使用 # 路徑模式
+  history: createWebHistory("/storyCard/"), 
   routes, // 導入使用上方 routes 所定義的路徑
 });
 
